4b5be5285d77231beeb06065c5292d5a83f76026
tao <864978550@qq.com>
2021-01-22 14:45:32 +0800

    test_go.mod

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	db, err := gorm.Open("mysql", "tao:12345678@/users")
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&model.User{})
+	r := gin.Default()
+
+}
--- model/user.go
+++ model/user.go
@@ -1,1 +1,9 @@
 package model
+
+type User struct {
+	StudentID   string `json:"student_id"`
+	Name        string `json:"name"`
+	Password    string `json:"password:`
+	UserPicture string `json:"user_picture"`
+	Gold        int    `json:"gold"`
+}
