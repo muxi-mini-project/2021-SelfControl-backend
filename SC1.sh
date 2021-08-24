aef3f45c3bc12dab5e69d906b4fccd291ddd9279
TAODEI <864978550@qq.com>
2021-08-25 02:44:34 +0800

    gitignore

--- SC1.sh
+++ SC1.sh
@@ -4,5 +4,5 @@ ip=39.99.53.8
 filename=$1
 
 go build -o bin/${filename} main.go 
-scp ./${filename} root@${ip}:/root/2021-SelfControl-backend
+scp bin/${filename} root@${ip}:/root/2021-SelfControl-backend
 ssh root@${ip}
